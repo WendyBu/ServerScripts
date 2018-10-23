b9cb6b6d542363c6435fdc335772494f3694f22d
yiwenbu <wybyw@hotmail.com>
2018-10-23 14:03:51 -0500

    add option username, password

--- curl_password.py
+++ curl_password.py
@@ -25,19 +25,32 @@ def generate_pbs(cmd, binPath, binName):
 
 # curl ftp://128.120.88.242/C202SC18081457/README.txt --user P202SC18081455-01_20181014_Qbqeu9:DoGkOL -o ./README.txt
 
-def main(argv1):
-    fastq_bin = "/archive2/tmhyxb9/MMC/fastq_bin"
+def main():
+    ftp_file = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
+
+    project_root = os.path.dirname(ftp_file)
+    fastq_bin = os.path.join(project_root, "fastq_bin")
+    fastq_dir = os.path.join(project_root, "fastq")
+    if not os.path.exists(fastq_bin):
+        os.mkdir(fastq_bin)
+    if not os.path.exists(fastq_dir):
+        os.mkdir(fastq_dir)
+
     # Open a file
-    with open(argv1) as f:
+    with open(ftp_file) as f:
         ftplist = f.read().splitlines()
         for ftplink in ftplist:
             filename = os.path.split(ftplink)[1]
-            cmd = "curl " + ftplink + " --user P202SC18081455-01_20181014_Qbqeu9:DoGkOL -o ./fastq/" + filename
+            cmd = "curl " + ftplink + " --user " + username + ":" + password + " -o ./fastq/" + filename
             generate_pbs(cmd, fastq_bin, filename)
     return
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
 
-# python /archive2/tmhyxb9/ToolBox/curl_password.py /archive2/tmhyxb9/MMC/fastq_ftplinks.txt
+# python /archive2/tmhyxb9/ToolBox/curl_password.py /archive2/tmhyxb9/FBL/fastq_ftplinks.txt P202SC18100591-01_20181022_guCAtO uKuyzk
+# Usage: goto the project root.
+# python /archive2/tmhyxb9/ToolBox/curl_password.py /archive2/tmhyxb9/FBL/fastq_ftplinks.txt username password
