bb0ed5c90d8379dc8c4b0337db0a8b1ec91c415f
yiwenbu <wybyw@hotmail.com>
2018-11-06 15:33:36 -0600

    revise bowtie

--- bowtie.py
+++ bowtie.py
@@ -1,43 +1,67 @@
 ###  data not from geo. allready data saved as fastq.gz
 ###  step1: unzip first
 ###  step2: run tophat
+###  step3: run bowtie
 
 import os, sys, os.path
 import pandas as pd
 import glob
 
-def bowtie(filename):
-    base = os.path.basename(filename).split(".")[0]
-    dir = os.path.dirname(filename)
-    binpath = os.path.join("chipbin","bowtie_"+ base)
-    pbs = open(binpath +".pbs", "w")
+def generate_pbs(cmd, binName, binPath):
+    """
+    :param cmd: command,
+    :param binName:
+    :param binPath:
+    :return: pbs filename
+    """
+    binAbsName = os.path.join(binPath, binName)
+    pbs = open(binAbsName + ".pbs", "w")
     pbs.write("#!/bin/bash\n")
     pbs.write("#PBS -r n\n")
-    pbs.write("#PBS -o " + binpath + ".out\n")
-    pbs.write("#PBS -e " + binpath + ".err\n")
+    pbs.write("#PBS -o " + binAbsName + ".out\n")
+    pbs.write("#PBS -e " + binAbsName + ".err\n")
     pbs.write("#PBS -m e\n")
     pbs.write("#PBS -M ybu2@houstonmethodist.org\n")
     pbs.write("#PBS -l walltime=96:00:00\n")
     pbs.write("#PBS -l nodes=1:ppn=8\n")
-    pbs.write("#PBS -l pmem=6000mb\n")
+    pbs.write("#PBS -l pmem=10000mb\n")
     pbs.write("#PBS -q mediummem\n")
-    pbs.write("cd "+ dir + "\n")
-    pbs.write("bowtie -m 1 -p 8 --chunkmbs 512 --best /archive2/tmhyxb9/ref_data/hg19/bowtie/hg19 " + base + ".fastq " + "/archive2/tmhyxb9/AY/chip_bowtie/" + base + ".bowtie"  + "\n")
+    pbs.write("module load python/2.7.11\n")
+    pbs.write(cmd + "\n")
     pbs.close()
-    os.chdir("/archive2/tmhyxb9/AY/")
-    os.system('qsub '+ binpath + '.pbs')
-    return
+    os.system('qsub ' + binAbsName + ".pbs")
+    pass
 
 
+def bowtie_cmd(file, outputDir):
+    cmd1 = "bowtie -m 1 -p 8 --chunkmbs 512 --best /archive2/tmhyxb9/ref_data/hg19/bowtie/hg19 "
+    cmd2 = file + " "
+    cmd3 = outputDir + "/"
+    cmd4 = os.path.basename(file).split(".")[0] + ".bowtie"
+    cmd = cmd1 + cmd2 + cmd3 + cmd4
+    return cmd
 
 #bowtie -m 1 -p 8 --chunkmbs 512 --best /home/tmhdxz9/scratch/reference/ref_data/hg19/bowtie/hg19 SRR6321859.fastq GSM2864676_mm9_brain_h3k27me3.bowtie
 
 def main():
-    for file in glob.glob("/archive2/tmhyxb9/AY/CHIP/*/*"):
-        bowtie(file)
+    fastq_dir = sys.argv[1]
+    if not os.path.exists("bowtie"):
+        os.mkdir("bowtie")
+    outputdir = os.path.join(os.getcwd(), "bowtie")
+    for file in glob.glob(fastq_dir + "/*/*.fastq"):
+        command = bowtie_cmd(file, outputdir)
+        binPath = os.path.join(os.getcwd(), "bowtie_bin")
+        if not os.path.exists(binPath):
+            os.mkdir(binPath)
+        binName = os.path.basename(file).split(".")[0] + "_bowtie"
+        generate_pbs(command, binName, binPath)
     return
 
 
 if __name__ == "__main__":
     main()
 
+
+# cd project root
+# root/fastq/sampleID/sample.fastq  ----structure.
+# python /archive2/tmhyxb9/ToolBox/chip_seq/bowtie.py  /archive2/tmhyxb9/Palb/Palb_pol2/fastq(input)
--- cuffdiff.py
+++ cuffdiff.py
@@ -35,13 +35,9 @@ def generate_pbs(cmd, binName, binPath):
     pass
 
 
-def run_cuffdiff(outputDir, labels, input_files, replicates=1):
-    cmdLabel = ",".join(labels)
-    cmdInput = " ".join(input_files)
-    cmd = "cuffdiff -p 9 --output-dir " + outputDir + " --labels "+ cmdLabel + " /archive2/tmhyxb9/ref_data/hg19/hg19.refGene.exon.anno.gtf "+ cmdInput
-    # print cmd
+def run_cuffdiff(outputDir, labels, input_files):
+    cmd = "cuffdiff -p 9 --output-dir " + outputDir + " --labels "+ labels + " /archive2/tmhyxb9/ref_data/hg19/hg19.refGene.exon.anno.gtf "+ input_files
     return cmd
-
 
 
 def main():
@@ -50,13 +46,11 @@ def main():
     output folder
     :return:
     """
-    inputFiles = []
-    labels = []
+    inputs = []
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "o:i:", ["output=", "input="])
+        opts, args = getopt.getopt(sys.argv[1:], "o:i:l:", ["output=", "input=", "label="])
     except getopt.GetoptError as err:
         print str(err)  # will print something like "option -a not recognized"
-        usage()
         sys.exit(2)
 
     for o, a in opts:
@@ -67,16 +61,25 @@ def main():
             binName = a
             # print "outputDir", outputDir
         elif o in ("-i", "--input"):
-            labels.append(a)
+            inputs.append(a)
+        elif o in ("-l", "--label"):
+            labels = a
         else:
             assert False, "unhandled option"
-    # print "labels", labels
-    for label in labels:
-        inputFile = os.path.join(cwdir, label,  "accepted_hits.bam")
-        inputFiles.append(inputFile)
-    # print "inputFiles", inputFiles
-    command = run_cuffdiff(outputDir, labels, inputFiles, 1) # use absolute path for each input file
 
+    # generate input commands
+    inputCommands = ""
+    for input in inputs:    # for each item after -i
+        input_sub = input.split(",")
+        inputFiles = ""
+        for input_single_dir in input_sub:
+            inputFile = os.path.join(cwdir, input_single_dir,  "accepted_hits.bam")
+            inputFiles += inputFile + ","
+        inputFiles = inputFiles[0:-1]
+        inputCommands += inputFiles + " "
+
+    command = run_cuffdiff(outputDir, labels, inputCommands) # use absolute path for each input file
+    #
     if not os.path.exists(outputDir):
         os.mkdir(outputDir)
     binpath = os.path.join(project_root, "cuffdiff_bin")
@@ -95,3 +98,6 @@ if __name__ == "__main__":
 # under tophat directory, giving the output folder name and the input folder name
 # python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_HT -i HT37_MMC -i HT42_MMC -i HT_37C -i HT_42C
 # python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_SNU -i SNU37MMC -i SNU42MMC -i SNU_37C -i SNU_42C
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_EZH2 -l control,EZH2_sh1,EZH2_sh2  -i Ctr_sh,Ctr2_sh -i EZH2_sh1,EZH22_sh1 -i EZH2_sh2,EZH22_sh2
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_FBL -l control,FBL  -i Ctr_sh,Ctr2_sh -i FBL_sh,FBL2_sh
+
