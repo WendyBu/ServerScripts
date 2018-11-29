109eefb927462c7d903cc92c51fe2abe9675c881
yiwenbu <wybyw@hotmail.com>
2018-11-29 17:53:03 -0600

    choose reference

--- cuffdiff.py
+++ cuffdiff.py
@@ -7,37 +7,14 @@ parse folders, each folder represent one group of samples
 import os , sys, getopt
 import pandas as pd
 import glob, os.path
+sys.path.insert(0, "/archive2/tmhyxb9/ToolBox")
+import cmd
 
 
-
-def generate_pbs(cmd, binName, binPath):
-    """
-    :param cmd: command,
-    :param binName:
-    :param binPath:
-    :return: pbs filename
-    """
-    binAbsName = os.path.join(binPath, binName)
-    pbs = open(binAbsName + ".pbs", "w")
-    pbs.write("#!/bin/bash\n")
-    pbs.write("#PBS -r n\n")
-    pbs.write("#PBS -o " + binAbsName + ".out\n")
-    pbs.write("#PBS -e " + binAbsName + ".err\n")
-    pbs.write("#PBS -m e\n")
-    pbs.write("#PBS -M ybu2@houstonmethodist.org\n")
-    pbs.write("#PBS -l walltime=96:00:00\n")
-    pbs.write("#PBS -l nodes=1:ppn=8\n")
-    pbs.write("#PBS -l pmem=6000mb\n")
-    pbs.write("#PBS -q mediummem\n")
-    pbs.write(cmd + "\n")
-    pbs.close()
-    os.system('qsub ' + binAbsName + ".pbs")
-    pass
-
-
-def run_cuffdiff(outputDir, labels, input_files):
-    cmd = "cuffdiff -p 9 --output-dir " + outputDir + " --labels "+ labels + " /archive2/tmhyxb9/ref_data/hg19/hg19.refGene.exon.anno.gtf "+ input_files
-    return cmd
+def run_cuffdiff(outputDir, labels, input_files, ref):
+    cmd = "cuffdiff -p 9 --output-dir " + outputDir + " --labels "+ labels + " " + ref + " " + input_files
+    cmds = [cmd]
+    return cmds
 
 
 def main():
@@ -48,7 +25,7 @@ def main():
     """
     inputs = []
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "o:i:l:", ["output=", "input=", "label="])
+        opts, args = getopt.getopt(sys.argv[1:], "o:i:l:r:", ["output=", "input=", "label=", "ref="])
     except getopt.GetoptError as err:
         print str(err)  # will print something like "option -a not recognized"
         sys.exit(2)
@@ -64,6 +41,8 @@ def main():
             inputs.append(a)
         elif o in ("-l", "--label"):
             labels = a
+        elif o in ("-r", "--ref"):
+            gene_reference = a
         else:
             assert False, "unhandled option"
 
@@ -78,14 +57,25 @@ def main():
         inputFiles = inputFiles[0:-1]
         inputCommands += inputFiles + " "
 
-    command = run_cuffdiff(outputDir, labels, inputCommands) # use absolute path for each input file
+    # choose reference, either ucsc or refseq
+    if gene_reference == "ucsc":
+        reference = "/archive2/tmhyxb9/ref_data/hg19/hg19.ucscgenes.knowngene.exon.anno.gtf"
+    elif gene_reference == "refseq":
+        reference = "/archive2/tmhyxb9/ref_data/hg19/hg19.refGene.exon.anno.gtf"
+    else:
+        print "gene reference cannot be recognized!"
 
+
+    cmds = run_cuffdiff(outputDir, labels, inputCommands, reference) # use absolute path for each input file
+
+    # make output dir and bin dir
     if not os.path.exists(outputDir):
         os.mkdir(outputDir)
     binpath = os.path.join(project_root, "cuffdiff_bin")
     if not os.path.exists(binpath):
         os.mkdir(binpath)
-    generate_pbs(command, binName, binpath)
+
+    cmd.generate_pbs(cmds=cmds, binName=binName, binPath=binpath)  # change to generate_submit_pbs to submit jobs
     pass
 
 
@@ -96,8 +86,14 @@ if __name__ == "__main__":
 
 # cuffdiff -p 9 --output-dir cuffdiffout_directory --labels foldernames,  /archive/tmhkxc48/ref_data/hg19/hg19.refGene.exon.anno.gtf control1/accepted_hits.bam,control2/accepted_hits.bam treat1/accepted_hits.bam,treat2/accepted_hits.bam
 # under tophat directory, giving the output folder name and the input folder name
+# the cffdiff output fold will be built under project root, same level as tophat.
 # python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_HT -i HT37_MMC -i HT42_MMC -i HT_37C -i HT_42C
 # python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_SNU -i SNU37MMC -i SNU42MMC -i SNU_37C -i SNU_42C
-# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_EZH2 -l control,EZH2_sh1,EZH2_sh2  -i Ctr_sh,Ctr2_sh -i EZH2_sh1,EZH22_sh1 -i EZH2_sh2,EZH22_sh2
-# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_FBL -l control,FBL  -i Ctr_sh,Ctr2_sh -i FBL_sh,FBL2_sh
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_EZH2 -l control,EZH2_sh1,EZH2_sh2  -i B1,B2 -i B5,B6 -i B7,B8
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_FBL -l control,FBL  -i B1,B2 -i B3,B4
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_EZH2 -l control,EZH2_sh1,EZH2_sh2  -i A1,A2 -i A3,A4 -i A5,A6
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_ADAR1 -l control,ADAR_sh1,ADAR_sh2  -i A1,A2 -i A7,A8 -i A9,A10
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_ADAR2 -l control,ADAR2  -i A1,A2 -i A11,A12
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_FBL -l control,FBL  -i Control -i FBL
+# python /archive2/tmhyxb9/ToolBox/cuffdiff.py -o cuffdiff_EZH2 -l control,EZH2sh1,EZH2sh2  -i Control -i EZH2sh1 -i EZH2sh2 -r ucsc
 
